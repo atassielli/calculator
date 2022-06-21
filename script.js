9ffaa8308d4aa5482adc927d27aedb9300e0cd66
atassielli <anthonytassielli@gmail.com>
2022-06-20 20:30:33 -0700

    Add number event listeners and create display array
    clicking the numbers will now add the number onto the display for the user to see

--- script.js
+++ script.js
@@ -1,39 +1,92 @@
 let answer;
 let a = 2;
 let b = 4;
+let firstNumber;
+let secondNumber;
+let operator;
+let getNumbers = [];
 
-function add () {
-    answer = a + b;
-    console.log('add' + answer);
-    return answer;
+const one = document.querySelector('.one');
+one.addEventListener('click', () => {
+    getNumbers.push(1);
+    display.textContent = getNumbers.join('');
+});
+
+const two = document.querySelector('.two');
+two.addEventListener('click', () => {
+    getNumbers.push(2);
+    display.textContent = getNumbers.join('');
+});
+
+const three = document.querySelector('.three');
+three.addEventListener('click', () => {
+    getNumbers.push(3);
+    display.textContent = getNumbers.join('');
+});
+
+const four = document.querySelector('.four');
+four.addEventListener('click', () => {
+    getNumbers.push(4);
+    display.textContent = getNumbers.join('');
+});
+
+const five = document.querySelector('.five');
+five.addEventListener('click', () => {
+    getNumbers.push(5);
+    display.textContent = getNumbers.join('');
+});
+
+const six = document.querySelector('.six');
+six.addEventListener('click', () => {
+    getNumbers.push(6);
+    display.textContent = getNumbers.join('');
+});
+
+const seven = document.querySelector('.seven');
+seven.addEventListener('click', () => {
+    getNumbers.push(7);
+    display.textContent = getNumbers.join('');
+});
+
+const eight = document.querySelector('.eight');
+eight.addEventListener('click', () => {
+    getNumbers.push(8);
+    display.textContent = getNumbers.join('');
+});
+
+const nine = document.querySelector('.nine');
+nine.addEventListener('click', () => {
+    getNumbers.push(9);
+    display.textContent = getNumbers.join('');
+});
+
+const display = document.querySelector('.display');
+display.textContent = getNumbers;
+
+function add (a,b) {
+    return a + b;
 }
 
-function subtract () {
-    answer = a - b;
-    console.log('subtract' + answer);
-    return answer;
+function subtract (a,b) {
+    return a - b;
 }
 
-function multiply () {
-    answer = a * b;
-    console.log('multiply' + answer);
-    return answer;
+function multiply (a,b) {
+    return a * b;
 }
 
-function divide () {
-    answer = a / b;
-    console.log('divide' + answer);
-    return answer;
+function divide (a,b) {
+    return a / b;
 }
 
 function operate () {
-    if ('user selected add') {
+    if (operator === addition) {
         add(firstNumber, secondNumber);
-    } else if ('user selected subtract') {
-        subtract(firstNumber, secondNumber)
-    } else if ('user selected multiply') {
+    } else if (operator === subtraction) {
+        subtract(firstNumber, secondNumber);
+    } else if (operator === multiplication) {
         multiply(firstNumber, secondNumber);
-    } else if ('user selected divide') {
+    } else if (operator === division) {
         divide(firstNumber, secondNumber);
     }
 }
@@ -43,3 +96,4 @@ subtract();
 multiply();
 divide();
 
+console.log(add(a,b))
