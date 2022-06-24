5faa2d571e34face9e6d9fe5a3499ec23979d744
atassielli <anthonytassielli@gmail.com>
2022-06-23 20:24:24 -0700

    Create function to check the length of the display
    display an error message if the number gets too long for the computer screen

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let answer;
-let a = 2;
-let b = 4;
 let firstNumber;
 let secondNumber;
 let operator = [];
@@ -8,69 +5,81 @@ let getNumbers = [];
 
 const decimal = document.querySelector('.decimal');
 decimal.addEventListener('click', () => {
-    getNumbers.push('.');
-    display.textContent = getNumbers.join('');
+    if (getNumbers.includes('.')) {
+        display.textContent = 'ERROR';
+    } else {
+        getNumbers.push('.');
+        checkLength();
+    }
 })
 
 const one = document.querySelector('.one');
 one.addEventListener('click', () => {
     getNumbers.push(1);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
 
 const two = document.querySelector('.two');
 two.addEventListener('click', () => {
     getNumbers.push(2);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
 
 const three = document.querySelector('.three');
 three.addEventListener('click', () => {
     getNumbers.push(3);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
 
 const four = document.querySelector('.four');
 four.addEventListener('click', () => {
     getNumbers.push(4);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
 
 const five = document.querySelector('.five');
 five.addEventListener('click', () => {
     getNumbers.push(5);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
 
 const six = document.querySelector('.six');
 six.addEventListener('click', () => {
     getNumbers.push(6);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
 
 const seven = document.querySelector('.seven');
 seven.addEventListener('click', () => {
     getNumbers.push(7);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
 
 const eight = document.querySelector('.eight');
 eight.addEventListener('click', () => {
     getNumbers.push(8);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
 
 const nine = document.querySelector('.nine');
 nine.addEventListener('click', () => {
     getNumbers.push(9);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
 
 const zero = document.querySelector('.zero');
 zero.addEventListener('click', () => {
     getNumbers.push(0);
-    display.textContent = getNumbers.join('');
+    checkLength();
 });
+
+function checkLength() {
+    if (getNumbers.length > 8) {
+        display.textContent = 'ERROR';
+    } else {
+        display.textContent = getNumbers.join('');
+    }
+}
 
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', () => {
